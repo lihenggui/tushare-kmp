3fdbb1de96735a6a84dc63dec12dd5a838c5a351
Mercury Li <contact@mercury.li>
2025-03-27 11:51:42 -0700

    Extract category to an enum class

--- library/src/commonMain/kotlin/li/mercury/tushare/api/index/models/IndexBasicParams.kt
+++ library/src/commonMain/kotlin/li/mercury/tushare/api/index/models/IndexBasicParams.kt
@@ -3,6 +3,7 @@ package li.mercury.tushare.api.index.models
 import kotlinx.serialization.Serializable
 import li.mercury.tushare.models.Market
 
+
 /**
  * 指数基本信息API请求参数
  */
@@ -17,5 +18,5 @@ data class IndexBasicParams(
     /** 发布商，如申万、中证等 */
     val publisher: String? = null,
     /** 指数类别，如主题指数、行业指数等 */
-    val category: String? = null
+    val category: IndexCategory? = null
 ) 
---
+++ library/src/commonMain/kotlin/li/mercury/tushare/api/index/models/IndexCategory.kt
@@ -0,0 +1,21 @@
+package li.mercury.tushare.api.index.models
+
+import kotlinx.serialization.Serializable
+
+/**
+ * 指数类别枚举
+ */
+@Suppress("NonAsciiCharacters")
+@Serializable
+enum class IndexCategory {
+    主题指数,
+    行业指数,
+    大盘指数,
+    风格指数,
+    成长指数,
+    价值指数,
+    基本面指数,
+    策略指数,
+    综合指数,
+    其他指数
+}
