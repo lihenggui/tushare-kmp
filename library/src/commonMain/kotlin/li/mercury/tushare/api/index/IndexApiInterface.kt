7d9c51e68fd6721598762456a2f042d8545b7f21
Mercury Li <contact@mercury.li>
2025-03-27 12:17:15 -0700

    Add IndexDaily

--- library/src/commonMain/kotlin/li/mercury/tushare/api/index/IndexApiInterface.kt
+++ library/src/commonMain/kotlin/li/mercury/tushare/api/index/IndexApiInterface.kt
@@ -3,6 +3,8 @@ package li.mercury.tushare.api.index
 import kotlinx.coroutines.flow.Flow
 import li.mercury.tushare.api.index.models.IndexBasicParams
 import li.mercury.tushare.api.index.models.IndexBasicResult
+import li.mercury.tushare.api.index.models.IndexDailyParams
+import li.mercury.tushare.api.index.models.IndexDailyResult
 
 /**
  * 指数相关API的存储库接口
@@ -17,4 +19,12 @@ interface IndexApiInterface {
      * @return 返回包含指数基本信息的Flow流
      */
     fun getIndexBasic(params: IndexBasicParams): Flow<List<IndexBasicResult>>
+    
+    /**
+     * 获取指数日线行情
+     *
+     * @param params 指数日线行情查询参数
+     * @return 返回包含指数日线行情的Flow流
+     */
+    fun getIndexDaily(params: IndexDailyParams): Flow<List<IndexDailyResult>>
 } 
