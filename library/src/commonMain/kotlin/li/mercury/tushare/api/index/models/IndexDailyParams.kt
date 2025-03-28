6bb44c8400d87f4ee67029d67ea4157b7498bf8d
Mercury Li <contact@mercury.li>
2025-03-28 15:51:14 -0700

    Add docs for index daily

--- library/src/commonMain/kotlin/li/mercury/tushare/api/index/models/IndexDailyParams.kt
+++ library/src/commonMain/kotlin/li/mercury/tushare/api/index/models/IndexDailyParams.kt
@@ -6,24 +6,37 @@ import li.mercury.tushare.utils.LocalDateAsStringSerializer
 
 /**
  * 指数日线行情API请求参数
- *
- * | 名称        | 类型 | 必选 | 描述 |
- * |------------|------|------|------|
- * | ts_code    | str  | Y    | 指数代码，来源指数基础信息接口 |
- * | trade_date | str  | N    | 交易日期 （日期格式：YYYYMMDD，下同） |
- * | start_date | str  | N    | 开始日期 |
- * | end_date   | str  | N    | 结束日期 |
  */
 @Serializable
 data class IndexDailyParams(
-    /** 指数代码，来源指数基础信息接口 */
+    /** 
+     * 指数代码
+     * 
+     * 必选参数，来源指数基础信息接口
+     */
     val tsCode: String,
-    /** 交易日期（日期格式：YYYYMMDD） */
+
+    /**
+     * 交易日期
+     * 
+     * 可选参数，日期格式：YYYYMMDD
+     */
     @Serializable(with = LocalDateAsStringSerializer::class)
     val tradeDate: LocalDate? = null,
-    /** 开始日期（日期格式：YYYYMMDD）*/
+
+    /**
+     * 开始日期
+     * 
+     * 可选参数，日期格式：YYYYMMDD
+     */
     @Serializable(with = LocalDateAsStringSerializer::class)
     val startDate: LocalDate? = null,
-    /** 结束日期 */
-    val endDate: String? = null,
+
+    /**
+     * 结束日期
+     * 
+     * 可选参数，日期格式：YYYYMMDD
+     */
+    @Serializable(with = LocalDateAsStringSerializer::class)
+    val endDate: LocalDate? = null,
 )
