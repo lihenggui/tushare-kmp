e737509a6fb0093f6f6ca3f5846c258547c5cea5
Mercury <contact@mercury.li>
2025-06-09 18:18:32 -0700

    Spotless

--- library/src/commonMain/kotlin/li/mercury/tushare/api/stock/board/models/KplListTag.kt
+++ library/src/commonMain/kotlin/li/mercury/tushare/api/stock/board/models/KplListTag.kt
@@ -2,11 +2,12 @@ package li.mercury.tushare.api.stock.board.models
 
 import kotlinx.serialization.Serializable
 
+@Suppress("NonAsciiCharacters", "ktlint:standard:enum-entry-name-case")
 @Serializable
 enum class KplListTag {
     涨停,
     炸板,
     跌停,
     自然涨停,
-    竞价
+    竞价,
 }
