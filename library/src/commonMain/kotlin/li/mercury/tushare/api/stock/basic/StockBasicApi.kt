e86a80d381b9f1b4d831cb67f332d61abb56e405
juliewww <julie.www333@gmail.com>
2025-06-12 16:31:27 +0800

    Refactor StockBasicApi to use suspend functions instead of Flow for API calls

--- library/src/commonMain/kotlin/li/mercury/tushare/api/stock/basic/StockBasicApi.kt
+++ library/src/commonMain/kotlin/li/mercury/tushare/api/stock/basic/StockBasicApi.kt
@@ -1,8 +1,8 @@
 package li.mercury.tushare.api.stock.basic
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import li.mercury.tushare.TuShare
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import li.mercury.tushare.api.stock.basic.models.BakBasicParams
 import li.mercury.tushare.api.stock.basic.models.BakBasicResult
 import li.mercury.tushare.api.stock.basic.models.HsConstParams
@@ -23,160 +23,84 @@ import li.mercury.tushare.api.stock.basic.models.StockCompanyParams
 import li.mercury.tushare.api.stock.basic.models.StockCompanyResult
 import li.mercury.tushare.api.stock.basic.models.TradeCalParams
 import li.mercury.tushare.api.stock.basic.models.TradeCalResult
+import li.mercury.tushare.http.HttpRequester
+import li.mercury.tushare.http.createRequest
+import li.mercury.tushare.http.perform
 import li.mercury.tushare.utils.toApiParams
 
 /**
  * 股票相关API的实现类
  */
 internal class StockBasicApi(
-    private val tuShare: TuShare,
+    private val requester: HttpRequester,
 ) : StockBasicApiInterface {
     /**
      * 获取股票基本信息
      * @param params 股票基本信息参数
-     * @return 股票基本信息数据流
+     * @return 股票基本信息列表
      */
-    override fun getStockBasic(params: StockBasicParams): Flow<List<StockBasicResult>> =
-        flow {
-            val apiParams = params.toApiParams()
-
-            val response =
-                tuShare.callApi(
-                    apiName = "stock_basic",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(StockBasicResult.serializer())
-            emit(results)
-        }
+    override suspend fun getStockBasic(params: StockBasicParams): List<StockBasicResult> {
+        val request = requester.createRequest("stock_basic", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
     /**
      * 获取沪深股通成份股数据
      * @param params 沪深股通成份股参数
-     * @return 沪深股通成份股数据流
+     * @return 沪深股通成份股数据列表
      */
-    override fun getHsConst(params: HsConstParams): Flow<List<HsConstResult>> =
-        flow {
-            val apiParams = params.toApiParams()
-
-            val response =
-                tuShare.callApi(
-                    apiName = "hs_const",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(HsConstResult.serializer())
-            emit(results)
-        }
+    override suspend fun getHsConst(params: HsConstParams): List<HsConstResult> {
+        val request = requester.createRequest("hs_const", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
     /**
      * 获取股票曾用名信息
      * @param params 股票曾用名参数
-     * @return 股票曾用名数据流
+     * @return 股票曾用名数据列表
      */
-    override fun getNameChange(params: NameChangeParams): Flow<List<NameChangeResult>> =
-        flow {
-            val apiParams = params.toApiParams()
-
-            val response =
-                tuShare.callApi(
-                    apiName = "namechange",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(NameChangeResult.serializer())
-            emit(results)
-        }
+    override suspend fun getNameChange(params: NameChangeParams): List<NameChangeResult> {
+        val request = requester.createRequest("namechange", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
     /**
      * 获取上市公司基本信息
      * @param params 上市公司基本信息参数
-     * @return 上市公司基本信息数据流
+     * @return 上市公司基本信息数据列表
      */
-    override fun getStockCompany(params: StockCompanyParams): Flow<List<StockCompanyResult>> =
-        flow {
-            val apiParams = params.toApiParams()
+    override suspend fun getStockCompany(params: StockCompanyParams): List<StockCompanyResult> {
+        val request = requester.createRequest("stock_company", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
-            val response =
-                tuShare.callApi(
-                    apiName = "stock_company",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(StockCompanyResult.serializer())
-            emit(results)
-        }
+    override suspend fun getStkPremarket(params: StkPremarketParams): List<StkPremarketResult> {
+        val request = requester.createRequest("stk_premarket", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
-    override fun getStkPremarket(params: StkPremarketParams): Flow<List<StkPremarketResult>> =
-        flow {
-            val apiParams = params.toApiParams()
+    override suspend fun getTradeCal(params: TradeCalParams): List<TradeCalResult> {
+        val request = requester.createRequest("trade_cal", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
-            val response =
-                tuShare.callApi(
-                    apiName = "stk_premarket",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(StkPremarketResult.serializer())
-            emit(results)
-        }
+    override suspend fun getStkManagers(params: StkManagersParams): List<StkManagersResult> {
+        val request = requester.createRequest("stk_managers", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
-    override fun getTradeCal(params: TradeCalParams): Flow<List<TradeCalResult>> =
-        flow {
-            val apiParams = params.toApiParams()
-            val response =
-                tuShare.callApi(
-                    apiName = "trade_cal",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(TradeCalResult.serializer())
-            emit(results)
-        }
+    override suspend fun getStkRewards(params: StkRewardsParams): List<StkRewardsResult> {
+        val request = requester.createRequest("stk_rewards", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
-    override fun getStkManagers(params: StkManagersParams): Flow<List<StkManagersResult>> =
-        flow {
-            val apiParams = params.toApiParams()
+    override suspend fun getNewShare(params: NewShareParams): List<NewShareResult> {
+        val request = requester.createRequest("new_share", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 
-            val response =
-                tuShare.callApi(
-                    apiName = "stk_managers",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(StkManagersResult.serializer())
-            emit(results)
-        }
-
-    override fun getStkRewards(params: StkRewardsParams): Flow<List<StkRewardsResult>> =
-        flow {
-            val apiParams = params.toApiParams()
-
-            val response =
-                tuShare.callApi(
-                    apiName = "stk_rewards",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(StkRewardsResult.serializer())
-            emit(results)
-        }
-
-    override fun getNewShare(params: NewShareParams): Flow<List<NewShareResult>> =
-        flow {
-            val apiParams = params.toApiParams()
-
-            val response =
-                tuShare.callApi(
-                    apiName = "new_share",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(NewShareResult.serializer())
-            emit(results)
-        }
-
-    override fun getBakBasic(params: BakBasicParams): Flow<List<BakBasicResult>> =
-        flow {
-            val apiParams = params.toApiParams()
-
-            val response =
-                tuShare.callApi(
-                    apiName = "bak_basic",
-                    params = apiParams,
-                )
-            val results = response.getResponseItems(BakBasicResult.serializer())
-            emit(results)
-        }
+    override suspend fun getBakBasic(params: BakBasicParams): List<BakBasicResult> {
+        val request = requester.createRequest("bak_basic", params.toApiParams())
+        return requester.perform { it.post { setBody(request) }.body() }
+    }
 }
